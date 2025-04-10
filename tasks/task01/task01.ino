3063d6971b281a521943c80e947585456f28515b
Pavel Kladiev <99913102+pkladiev@users.noreply.github.com>
2025-04-10 14:27:26 +0400

    Done task 1

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,23 @@
-void setup() {
+const int pins[] = {3, 5, 6, 9, 10};
+const unsigned long intervals[] = {10000, 1000, 500, 100, 50};
+bool states[] = {LOW, LOW, LOW, LOW, LOW};
+unsigned long lastToggle[] = {0, 0, 0, 0, 0};
+const int pinCount = 5;
 
+void setup() {
+  for (int i = 0; i < pinCount; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
-
+  unsigned long now = micros();
+  
+  for (int i = 0; i < pinCount; i++) {
+    if (now - lastToggle[i] >= intervals[i]) {
+      lastToggle[i] = now;
+      states[i] = !states[i];
+      digitalWrite(pins[i], states[i]);
+    }
+  }
 }
